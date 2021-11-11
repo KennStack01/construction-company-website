35d4f299ccfa54f274a0013d41c287ff039288b2
Kenn Stack <kibadikennedy@gmail.com>
2021-11-11 14:39:19 +0800

    added clients list

--- src/components/Home/ClientsList.js
+++ src/components/Home/ClientsList.js
@@ -25,15 +25,15 @@ const ClientsList = () => {
       <h1 className="text-center font-semibold text-2xl md:text-4xl text-gray-700 my-14 md:mt-20">
         Ils nous font confiance
       </h1>
-      <div className="flex flex-row justify-between md:grid grid-cols-4 mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 mx-auto">
         {clients.map(client => (
-          <div key={client.id} className="flex flex-col justify-center">
+          <div key={client.id} className="flex flex-col justify-center my-2">
             <img
               src={client.clientLogo.url}
               alt={client.clientCompanyName}
-              className="w-auto h-32 mx-auto md:w-auto md:h-32 filter grayscale"
+              className="w-auto h-16 mx-auto md:w-auto filter grayscale"
             />
-            <h5 className="text-gray-700 text-md font-semibold mx-auto">
+            <h5 className="text-gray-700 text-sm font-semibold mx-auto">
               {" "}
               {client.clientCompanyName}{" "}
             </h5>
