993954751d47f9fe8c90e22bda4df7c1d969cf21
Kenn Stack <kibadikennedy@gmail.com>
2021-11-15 19:36:03 +0800

    added contact page content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,20 @@
 import React from "react"
+import ContactInfo from "../components/Contact/ContactInfo"
+import Form from "../components/Contact/Form"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Contact = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Contact" />
 
-    <p className="text-sadi-blue-600 text-center text-2xl font-bold">
-      Welcome to Contact site.
-    </p>
+    <h1 className="text-xl md:text-3xl md:text-center my-10 md:my-16 text-sadi-blue-500 font-bold justify-self-start">
+      Nous sommes disponibles pour vous aider
+    </h1>
+    <div className="flex flex-col md:flex-row justify-center">
+      <Form />
+      <ContactInfo />
+    </div>
   </Layout>
 )
 
