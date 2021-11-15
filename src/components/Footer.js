c820ad618fdb6445c9f38ee5184dcab192ca7dee
Kenn Stack <kibadikennedy@gmail.com>
2021-11-15 20:35:05 +0800

    built the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,119 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-
-import { IoLogoTwitter, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5"
+import { BsFacebook, BsInstagram, BsYoutube, BsSunFill } from "react-icons/bs"
+import { FaMoon } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col md:flex-row justify-between bg-sadi-blue-700 py-10 px-3 md:p-4 w-full text-white font-sans">
-      {/* Logos Social Media */}
-      <div className="flex flex-row justify-evenly md:justify-evenly mb-5 md:my-auto font-semibold">
-        <a
-          href="https://www.linkedin.com/in/kennkibadi/"
-          target="__blank"
-          className="flex flex-row justify-center mx-3 text-white hover:text-turbo-500"
-        >
-          <div className="text-xl lg:text-2xl">
-            <IoLogoLinkedin />
+    <div className="divide-y divide-white text-white bg-sadi-blue-700 py-2 md:py-5 px-2 md:px-20 w-full">
+      <div className="flex flex-col">
+        <div className="grid grid-cols-2 place-content-center mx-auto md:mx-0 md:flex flex-row justify-center text-sm">
+          <div className="flex flex-col md:flex-row mx-2">
+            <div className="flex flex-col my-2 mx-6">
+              <h1 className="text-md md:text-xl font-semibold my-1 md:my-3">
+                Companie
+              </h1>
+              <div className="flex flex-col">
+                <Link to="/">
+                  <h5 className="hover:underline hover:bg-back-color-500 p-1 rounded">
+                    Acceuil
+                  </h5>
+                </Link>
+                <Link to="/services">
+                  <h5 className="hover:underline hover:bg-back-color-500 p-1 rounded">
+                    Services
+                  </h5>
+                </Link>
+                <Link to="/realisations">
+                  <h5 className="hover:underline hover:bg-back-color-500 p-1 rounded">
+                    Réalisations
+                  </h5>
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col my-2 mx-6">
+              <h1 className="text-md md:text-xl font-semibold my-1 md:my-3">
+                Informations
+              </h1>
+              <div className="flex flex-col">
+                <Link to="/actu">
+                  <h5 className="hover:underline hover:bg-back-color-500 p-1 rounded">
+                    Actualités
+                  </h5>
+                </Link>
+                <Link to="/contact">
+                  <h5 className="hover:underline hover:bg-back-color-500 p-1 rounded">
+                    Contact
+                  </h5>
+                </Link>
+              </div>
+            </div>
           </div>
-          <h1 className="text-sm md:text-md my-auto mx-1 font-sans">
-            LinkedIn
+
+          <div className="flex flex-col my-2 mx-6">
+            <h1 className="text-md md:text-xl font-semibold my-1 md:my-3">
+              Légal
+            </h1>
+            <div className="flex flex-col">
+              <Link to="/">
+                <h5 className="hover:underline p-1 rounded">
+                  Politique de confidentialité
+                </h5>
+              </Link>
+              <Link to="/">
+                <h5 className="hover:underline p-1 rounded">
+                  Conditions d’utilisation
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between mx-auto my-5 text-xl">
+          <h1 className="text-sm md:text-md font-semibold">
+            Retrouvez-nous sur:{" "}
           </h1>
-        </a>
-        <a
-          href="https://twitter.com/KennKIBADI"
-          target="__blank"
-          className="flex flex-row justify-center mx-3 text-white hover:text-turbo-500"
-        >
-          <div className="text-xl lg:text-2xl">
-            <IoLogoTwitter />
+          <div className="flex flex-row mx-auto">
+            <a
+              href="https://www.linkedin.com/in/kennkibadi/"
+              target="__blank"
+              className="mx-1 my-auto"
+            >
+              <div className="text-2xl lg:text-3xl my-auto">
+                <BsFacebook />
+              </div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kennkibadi/"
+              target="__blank"
+              className="mx-1 my-auto"
+            >
+              <div className="text-2xl lg:text-3xl my-auto">
+                <BsInstagram />
+              </div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kennkibadi/"
+              target="__blank"
+              className="mx-1 my-auto"
+            >
+              <div className="text-2xl lg:text-3xl my-auto">
+                <BsYoutube />
+              </div>
+            </a>
           </div>
-          <h1 className="text-sm md:text-md my-auto mx-1 font-sans">Twitter</h1>
-        </a>
-
+        </div>
+      </div>
+      <p className="mx-auto mt-2 md:mt-5 w-4/5 text-center text-xs md:text-md my-auto">
+        © 2021 Sadi SARL - Built by{" "}
         <a
-          href="https://github.com/KennStack01"
+          href="https://www.webcontract.io"
           target="__blank"
-          className="flex flex-row justify-center mx-3 text-white hover:text-turbo-500"
+          className="hover:text-yellow-400 font-semibold"
         >
-          <div className="text-xl lg:text-2xl">
-            <IoLogoGithub />
-          </div>
-          <h1 className="text-sm md:text-md my-auto mx-1 font-sans">Github</h1>
-        </a>
-      </div>
-
-      <div className="md:my-auto my-5 text-sm md:text-md">
-        © {new Date().getFullYear()}, {"  "}
-        <Link to="/services" className="font-medium hover:text-yellow-500">
-          {" "}
-          SADI sarl Business
-        </Link>
-      </div>
-    </footer>
+          WebContract.io
+        </a>{" "}
+      </p>
+    </div>
   )
 }
 
