55405e76e889ffc0253977e77d32d1b1fa093983
Kenn Stack <kibadikennedy@gmail.com>
2021-11-17 21:41:08 +0800

    first commit

--- src/components/Home/Mission.js
+++ src/components/Home/Mission.js
@@ -19,7 +19,8 @@ const Mission = () => {
     <div className="mission flex flex-col md:flex-row bg-white p-0 rounded-md shadow-sm pt-4 sm:pt-0 my-8 md:my-16 mx-4 mr-4 md:mx-20">
       <div className="flex flex-col justify-between my-auto mx-4 md:w-1/2">
         <h1 className="text-xl md:text-3xl md:text-center text-sadi-blue-500 font-bold justify-self-start">
-          {data.graphCmsMissionDescription.title}
+          {/* {data.graphCmsMissionDescription.title} */}
+          Test eeeeeeeeeeeeeeeh
         </h1>
         <p className="text-sm md:text-lg text-justify text-gray-700 font-medium my-6">
           {data.graphCmsMissionDescription.textDescription}
